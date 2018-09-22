960b1385728e4b9469c93442b06ca682e7154266
Tim Henkes <tim.henkes@student.hs-rm.de>
2018-09-22 19:28:24 +0200

    Changes to the before_install Travis CI script

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    echo "brew update"
+    brew update
+    echo "brew install pyenv"
+    brew install pyenv
+    echo "brew install pyenv-virtualenv"
     brew install pyenv-virtualenv
+    echo "pyenv install $PYTHON_VERSION"
+    pyenv install $PYTHON_VERSION
+    echo "pyenv virtualenv $PYTHON_VERSION virtualenv"
     pyenv virtualenv $PYTHON_VERSION virtualenv
+    echo "pyenv activate virtualenv"
     pyenv activate virtualenv
+    echo "python --version"
     python --version
+    echo "pip install --upgrade pip setuptools wheel"
     pip install --upgrade pip setuptools wheel
+    echo "pip --version"
     pip --version
 fi
