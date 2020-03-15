5b6b93b7f870a8e814b5a275a590088630c1c969
Tim Henkes <me@syndace.dev>
2020-03-15 23:23:01 +0100

    travis fixed

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -27,4 +27,10 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     pip install --upgrade pip setuptools wheel
     echo "> pip --version"
     pip --version
+
+    echo "> brew install libsodium"
+    brew install libsodium
+else
+    sudo apt update
+    sudo apt install libsodium18
 fi
