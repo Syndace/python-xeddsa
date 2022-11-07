bd4b19bae05c1d86e6ee5763b87ea590710bbe9e
Tim Henkes <me@syndace.dev>
2022-11-07 13:11:03 +0100

    Removed libxeddsa binaries from code and documentation, since wheels will replace the need for them

--- docs/installation.rst
+++ docs/installation.rst
@@ -3,7 +3,7 @@ Installation
 
 python-xeddsa depends on two system libraries, `libxeddsa <https://github.com/Syndace/libxeddsa>`__>=2,<3, and `libsodium <https://download.libsodium.org/doc/>`__.
 
-Install the latest release using pip (``pip install XEdDSA``) or manually from source by running ``pip install .`` (preferred) or ``python setup.py install`` in the cloned repository. The installation requires libsodium and the Python development headers to be installed. If a locally installed version of libxeddsa is available, python-xeddsa tries to use that. Otherwise it uses prebuilt binaries of the library, which are available for Linux, MacOS and Windows on the amd64 architecture, and potentially for MacOS arm64 too. Set the ``LIBXEDDSA_FORCE_LOCAL`` environment variable to forbid the usage of prebuilt binaries.
+Install the latest release using pip (``pip install XEdDSA``), from the wheels available in the artifacts of the `Test & Publish workflow <https://github.com/Syndace/python-xeddsa/actions/workflows/test-and-publish.yml>`_, or manually from source by running ``pip install .`` (preferred) or ``python setup.py install`` in the cloned repository. The installation from source requires libxeddsa, libsodium and the Python development headers to be installed.
 
 Usage with Brython
 ------------------
--- libxeddsa/build.py
+++ libxeddsa/build.py
@@ -1,15 +1,9 @@
 import ctypes.util
 import os
 import platform
-import sys
 from typing import Optional, Tuple
 
 import cffi  # type: ignore[import]
-
-ffibuilder = cffi.FFI()
-
-libxeddsa_prebuilt_dir = os.path.dirname(os.path.abspath(__file__))
-libxeddsa_header = os.path.join(libxeddsa_prebuilt_dir, "xeddsa.h")
 
 
 class LibraryNotFoundException(Exception):
@@ -49,81 +43,31 @@ def find_library(library: str) -> Tuple[Optional[str], str]:
     raise LibraryNotFoundException(f"Library {name} not found.")
 
 
-def find_libxeddsa() -> Tuple[Optional[str], str]:
-    """
-    Find libxeddsa, either a locally installed version, or one of the versions included with the package.
-
-    Returns:
-        A tuple, where the first entry contains the path that the library can be found in, and the second
-        entry is the name of the library. If `None` is returned for the path, the library is included in the
-        necessary search paths to load it without an explicit path.
-    """
-
-    try:
-        return find_library("xeddsa")
-    except LibraryNotFoundException as e:
-        if os.getenv("LIBXEDDSA_FORCE_LOCAL"):
-            raise Exception("No locally installed version of libxeddsa found.") from e
-
-    print(
-        "No locally installed version of libxeddsa found, falling back to prebuilt binaries. Set the"
-        " 'LIBXEDDSA_FORCE_LOCAL' environment variable to prevent usage of prebuilt binaries."
-    )
-
-    if sys.maxsize != 2 ** 63 - 1:
-        raise Exception(
-            "Prebuilt binaries of libxeddsa are not available for 32 bit Python interpreters. Build or"
-            " install libxeddsa and make sure it can be found by Python/your C compiler."
-        )
-
-    system = platform.system()
-    machine = platform.machine().lower()
-
-    is_amd64 = machine in { "amd64", "x86_64" }
-    is_arm64 = machine in { "arm64" }
-
-    if system == "Linux" and is_amd64:
-        return (libxeddsa_prebuilt_dir, "xeddsa-linux-amd64")
-    if system == "Darwin" and is_amd64:
-        return (libxeddsa_prebuilt_dir, "xeddsa-macos-amd64")
-    if system == "Darwin" and is_arm64:
-        return (libxeddsa_prebuilt_dir, "xeddsa-macos-arm64")
-    if system == "Windows" and is_amd64:
-        return (libxeddsa_prebuilt_dir, "libxeddsa-windows-amd64")
-
+try:
+    libxeddsa_dir, libxeddsa_library = find_library("xeddsa")
+except LibraryNotFoundException as e:
     raise Exception(
-        f"Prebuilt binaries of libxeddsa are not available for operating system '{system}' on architecture"
-        f" '{machine}'. Build or install libxeddsa and make sure it can be found by Python/your C compiler."
-    )
+        "No locally installed version of libxeddsa found. Make sure libxeddsa is installed and can be"
+        " found by Python/your C compiler."
+    ) from e
 
-
-def find_libsodium() -> Tuple[Optional[str], str]:
-    """
-    Find a locally installed version of libsodium.
-
-    Returns:
-        A tuple, where the first entry contains the path that the library can be found in, and the second
-        entry is the name of the library. If `None` is returned for the path, the library is included in the
-        necessary search paths to load it without an explicit path.
-    """
-
-    try:
-        return find_library("sodium")
-    except LibraryNotFoundException as e:
-        raise Exception(
-            "No locally installed version of libsodium found. Make sure libsodium is installed and can be"
-            " found by Python/your C compiler."
-        ) from e
-
-
-libxeddsa_dir, libxeddsa_library = find_libxeddsa()
-libsodium_dir, libsodium_library = find_libsodium()
+try:
+    libsodium_dir, libsodium_library = find_library("sodium")
+except LibraryNotFoundException as e:
+    raise Exception(
+        "No locally installed version of libsodium found. Make sure libsodium is installed and can be"
+        " found by Python/your C compiler."
+    ) from e
 
 library_dirs = []
 if libxeddsa_dir is not None:
     library_dirs.append(libxeddsa_dir)
 if libsodium_dir is not None:
     library_dirs.append(libsodium_dir)
+
+ffibuilder = cffi.FFI()
+
+libxeddsa_header = os.path.join(os.path.dirname(os.path.abspath(__file__)), "xeddsa.h")
 
 with open(libxeddsa_header, encoding="utf-8") as f:
     ffibuilder.cdef(f.read())
