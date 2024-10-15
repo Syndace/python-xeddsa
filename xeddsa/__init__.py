d53397544ecf4d49a8e5c2480a48bc184865d027
Tim Henkes <me@syndace.dev>
2024-10-15 06:29:28 +0200

    Switch to 'as' reexports in __init__.py thanks to new pylint option

--- xeddsa/__init__.py
+++ xeddsa/__init__.py
@@ -1,69 +1,30 @@
-from .version import __version__
-from .project import project
+from .version import __version__ as __version__
+from .project import project as project
 
 from .bindings import (
-    Priv, PRIV_SIZE,
-    Seed, SEED_SIZE,
-    Curve25519Pub, CURVE_25519_PUB_SIZE,
-    Ed25519Pub, ED_25519_PUB_SIZE,
-    Ed25519Signature, ED_25519_SIGNATURE_SIZE,
-    Nonce, NONCE_SIZE,
-    SharedSecret, SHARED_SECRET_SIZE,
+    Priv as Priv, PRIV_SIZE as PRIV_SIZE,
+    Seed as Seed, SEED_SIZE as SEED_SIZE,
+    Curve25519Pub as Curve25519Pub, CURVE_25519_PUB_SIZE as CURVE_25519_PUB_SIZE,
+    Ed25519Pub as Ed25519Pub, ED_25519_PUB_SIZE as ED_25519_PUB_SIZE,
+    Ed25519Signature as Ed25519Signature, ED_25519_SIGNATURE_SIZE as ED_25519_SIGNATURE_SIZE,
+    Nonce as Nonce, NONCE_SIZE as NONCE_SIZE,
+    SharedSecret as SharedSecret, SHARED_SECRET_SIZE as SHARED_SECRET_SIZE,
 
-    XEdDSAException,
+    XEdDSAException as XEdDSAException,
 
-    ed25519_priv_sign,
-    ed25519_seed_sign,
-    ed25519_verify,
+    ed25519_priv_sign as ed25519_priv_sign,
+    ed25519_seed_sign as ed25519_seed_sign,
+    ed25519_verify as ed25519_verify,
 
-    curve25519_pub_to_ed25519_pub,
-    ed25519_pub_to_curve25519_pub,
+    curve25519_pub_to_ed25519_pub as curve25519_pub_to_ed25519_pub,
+    ed25519_pub_to_curve25519_pub as ed25519_pub_to_curve25519_pub,
 
-    priv_to_curve25519_pub,
-    priv_to_ed25519_pub,
-    seed_to_ed25519_pub,
+    priv_to_curve25519_pub as priv_to_curve25519_pub,
+    priv_to_ed25519_pub as priv_to_ed25519_pub,
+    seed_to_ed25519_pub as seed_to_ed25519_pub,
 
-    priv_force_sign,
-    seed_to_priv,
+    priv_force_sign as priv_force_sign,
+    seed_to_priv as seed_to_priv,
 
-    x25519
+    x25519 as x25519
 )
-
-
-# Fun:
-# https://github.com/PyCQA/pylint/issues/6006
-# https://github.com/python/mypy/issues/10198
-__all__ = [  # pylint: disable=unused-variable
-    # .version
-    "__version__",
-
-    # .project
-    "project",
-
-    # .bindings
-    "Priv", "PRIV_SIZE",
-    "Seed", "SEED_SIZE",
-    "Curve25519Pub", "CURVE_25519_PUB_SIZE",
-    "Ed25519Pub", "ED_25519_PUB_SIZE",
-    "Ed25519Signature", "ED_25519_SIGNATURE_SIZE",
-    "Nonce", "NONCE_SIZE",
-    "SharedSecret", "SHARED_SECRET_SIZE",
-
-    "XEdDSAException",
-
-    "ed25519_priv_sign",
-    "ed25519_seed_sign",
-    "ed25519_verify",
-
-    "curve25519_pub_to_ed25519_pub",
-    "ed25519_pub_to_curve25519_pub",
-
-    "priv_to_curve25519_pub",
-    "priv_to_ed25519_pub",
-    "seed_to_ed25519_pub",
-
-    "priv_force_sign",
-    "seed_to_priv",
-
-    "x25519"
-]
